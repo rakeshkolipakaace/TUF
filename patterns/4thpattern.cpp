ab87867a93bab4ae21370148be446c6d7be04db3
rakeshkolipakaace <rakeshkolipaka2125@gmail.com>
2025-02-17 21:27:11 +0530

    commit

---
+++ patterns/4thpattern.cpp
@@ -0,0 +1,20 @@
+void triangle(int n) {
+	// Write your code here
+	 for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+        cout<<i<<" ";
+    }
+    cout<<endl;
+   }
+}
+
+/*
+output:
+
+1 
+2 2 
+3 3 3
+
+
+
+*/
---
+++ patterns/6th patterns.cpp
@@ -0,0 +1,21 @@
+void nNumberTriangle(int n) {
+    for(int i=1;i<=n;i++){
+		 for(int j=1;j<=n-(i-1);j++){
+			 cout<<j<<" ";
+		 }
+		 cout<<endl;
+	 }
+
+    // Write your code here.
+}
+
+
+/*
+Output:
+
+1 2 3 4
+1 2 3
+1 2
+1
+
+*/
